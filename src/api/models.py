c37c96735fc16aef41885e3de74467beccd17623
Johan Lopez <johanandrey0210@gmail.com>
2023-10-31 00:47:13 +0000

    Add Product Model and API Services

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(500), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Product
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,51 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/products', methods=['GET'])
+def get_products():
+
+    all_products = Product.query.all()
+    products_seriallize = [product.serialize() for product in all_products]
+
+    return jsonify(products_seriallize), 200
+
+@api.route('/product', methods=['POST'])
+def post_product():
+
+    body = request.json
+    new_product = Product(id=body['id'],name=body['name'],description=body['description'],price=body['price'],amount=body['amount'])
+    db.session.add(new_product)
+    db.session.commit()
+    
+    return jsonify({"message": "Producto creado con éxito"}), 200
+
+@api.route('/product/<id>', methods=['PUT'])
+def put_product(id):
+    product = Product.query.get(id)
+    body = request.json
+
+    if not product:
+        return jsonify({"message": "Producto no encontrado"}), 404
+    
+    product.name = body['name']
+    product.description = body['description']
+    product.price = body['price']
+    product.amount = body['amount']
+
+    db.session.commit()
+    
+    return jsonify({"message": "Producto modificado con éxito"}), 200
+
+@api.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify({"message": "Producto no encontrado"}), 404
+
+    db.session.delete(product)
+    db.session.commit()
+    
+    return jsonify({"message": "Producto eliminado con éxito"}), 200
