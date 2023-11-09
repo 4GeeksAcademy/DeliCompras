c01e5210beda686a9cea70d7cf288aa2c25a0711
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-11-09 04:59:24 -0600

    Update appContext.js

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -29,6 +29,7 @@ const injectContext = PassedComponent => {
 			 * store, instead use actions, like this:
 			state.actions.getCategorias();
 			state.actions.updateList();
+   			state.actions.getRestaurantes();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
