90e030260a25eba84a21487f68059b4a96405704
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-11-09 04:42:06 -0600

    Update admin.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Product, Categorias
+from .models import db, User, Product, Categorias, Restaurantes
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,6 +14,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Categorias, db.session))
     admin.add_view(ModelView(Product, db.session))
+    admin.add_view(ModelView(Restaurantes, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
