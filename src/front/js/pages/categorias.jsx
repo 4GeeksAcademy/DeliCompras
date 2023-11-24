f936b7aa307e390c312fea7fb6c7a6cf60e04093
Johan Lopez <johanandrey0210@gmail.com>
2023-11-24 19:45:37 +0000

    3ro

--- src/front/js/pages/categorias.jsx
+++ src/front/js/pages/categorias.jsx
@@ -24,7 +24,7 @@ export const Categorias = () => {
               </tr>
             </thead>
             <tbody>
-              {store.products.map((item) => (
+              {store.categories.map((item) => (
                 <tr style={{}}>
                   <td className="td" style={{paddingLeft:"30px"}}><img width="40" height="40" src={item.url_img} alt="Imagen Seleccionada" /></td>
                   <td className="td"><b style={{color:"#0aad0a"}}>{item.name}</b></td>
@@ -34,13 +34,15 @@ export const Categorias = () => {
                         <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
                       </svg>
                       <ul class="dropdown-menu" style={{borderRadius:"8px"}}>
-                        <li><a class="dropdown-item" href="#" style={{width:"90%", margin:"auto", borderRadius:"8px"}}>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16" style={{margin:" 2px 12px 5px 0px"}}>
-                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                          </svg>
-                          Editar
-                        </a></li>
-                        <li><a class="dropdown-item" href="#" style={{width:"90%", margin:"auto", borderRadius:"8px"}}>
+                        <Link to={`/modificar_categorias/${item.id}`}>
+                          <li><a class="dropdown-item" href="#" style={{width:"90%", margin:"auto", borderRadius:"8px"}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16" style={{margin:" 2px 12px 5px 0px"}}>
+                              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                            </svg>
+                            Editar
+                          </a></li>
+                        </Link>
+                        <li><a onClick={() => actions.deleteCategories(item.id)} class="dropdown-item" href="#" style={{width:"90%", margin:"auto", borderRadius:"8px"}}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style={{margin:" 2px 12px 5px 0px"}}>
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -54,7 +56,11 @@ export const Categorias = () => {
               ))}
             </tbody>
           </table>
-          <button onClick={() => actions.setSelectOpcion(5)} className="btn btn-success" style={{borderRadius:"8px" ,backgroundColor:"#0aad0a"}}><b>Nuevo Producto</b></button>
+          <div>
+            <Link to="/crear_categorias">
+              <button onClick={() => actions.setSelectOpcion(5)} className="btn btn-success" style={{borderRadius:"8px" ,backgroundColor:"#0aad0a"}}><b>Nueva Categoria</b></button>
+            </Link>
+          </div>
         </div>
       </div>
     }
--- src/front/js/pages/create_category.jsx
+++ src/front/js/pages/create_category.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link, Navigate } from "react-router-dom";
+import default_rest from "../../img/default_rest.jpg"
 
 export const Crear_categorias = () => { 
     const { store, actions } = useContext(Context);
@@ -36,29 +37,28 @@ export const Crear_categorias = () => {
     return (
         <>
         { !store.auth ? <Navigate to="/categorias" /> :
-            <div>
-                <img width="100" src={image ? URL.createObjectURL(image) : null } alt="Imagen Seleccionada" />
+            <div className="card" style={{margin:"5% 25%", padding:"40px", borderRadius: "12px"}}>
+                <div className="card-body">
+                    <div className="pb-2"><h1>Nuevo Producto</h1></div>
+                    <div className="mb-3" style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+                        <img style={{width:"200px", height:"200px"}} src={image ? URL.createObjectURL(image) : default_rest } alt="Imagen Seleccionada" />
 
-                <div className="mb-3">
-                    <label htmlFor="img" className="form-label">Imagen</label>
-                    <input
-                        id="img"
-                        type="file"
-                        accept="image/*"
-                        onChange={(e)=> {setImage(e.target.files[0])}}
-                    />
-                </div>
-                <div className="card" style={{width: "18rem"}}>
-                    <div className="card-body">
-                        <form>
-                            <div className="mb-3">
-                                <label htmlFor="name" className="form-label">Name</label>
-                                <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-                            </div>
+                        <input
+                            id="img"
+                            type="file"
+                            accept="image/*"
+                            onChange={(e)=> {setImage(e.target.files[0])}}
+                        />
+                    </div>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>     
 
-                            <button disabled={!isFormValid} onClick={crear}>Guardar Cambios</button>
-                            {create ? <Navigate to='/categorias' /> : null}
-                        </form>
+                    <div className="pt-3" style={{display:"flex", justifyContent:"end"}}>
+                        <button className="btn btn-success" style={{borderRadius:"8px" ,backgroundColor:"#0aad0a"}} disabled={!isFormValid} onClick={crear}><b>Guardar Cambios</b></button>
+                        {create ? <Navigate to='/categorias' /> : null}
                     </div>
                 </div>
             </div>
