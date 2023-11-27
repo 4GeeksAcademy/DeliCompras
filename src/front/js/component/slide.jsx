51d7be2e4e84189a04dd863e9d95095ab79c2fbf
Johan Lopez <johanandrey0210@gmail.com>
2023-11-27 03:04:18 +0000

    mejoras en las traducciones

--- src/front/js/component/slide.jsx
+++ src/front/js/component/slide.jsx
@@ -1,53 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import back1 from "../../img/back1.jpg"
 import back2 from "../../img/back2.jpg"
 
 export const Slide = () => {
     return (
         <div className="container" style={{paddingTop:"40px"}}>
-        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
+            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
 
-                <div className="carousel-item active" style={{backgroundImage: `url(${back1})`, backgroundSize: "cover", backgroundPosition: "center" , height:"540px", justifyContent:"start", alignItems:"center", borderRadius:"12px"}}>
-                    <div className="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center" style={{width:"400px", height:"320px" , marginLeft:"100px"}}>
-                        <span className="badge" style={{background:"#ffc107", color:"black"}}>Opening Sale Discount 50%</span>
+                <div className="carousel-inner">
+                    <div className="carousel-item active" style={{backgroundImage: `url(${back1})`, backgroundSize: "cover", backgroundPosition: "center" , height:"540px", justifyContent:"start", alignItems:"center", borderRadius:"12px"}}>
+                        <div className="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center" style={{width:"400px", height:"320px" , marginLeft:"100px"}}>
+                            <span className="badge" style={{background:"#ffc107", color:"black"}}>Descuento por Apertura del 50%</span>
 
-                        <h2 className="text-dark display-5 fw-bold mt-4">SuperMarket For Fresh Grocery</h2>
-                        <p className="lead">Introduced a new model for online grocery shopping and convenient home delivery.</p>
-                        <Link to="/lista_por_categorias/1">
-                            <button href="#!" className="btn btn-dark mt-3" tabIndex="-1">
-                                Shop Now
-                                <i className="feather-icon icon-arrow-right ms-1"></i>
-                            </button>
-                        </Link>
+                            <h2 className="text-dark display-5 fw-bold mt-4">Abastecedora de Alimentos</h2>
+                            <p className="lead">Compra tus Suministros con Seguridad y de forma Rapida.</p>
+                            <Link to="/lista_por_categorias/1">
+                                <button href="#!" className="btn btn-dark mt-3" tabIndex="-1">
+                                    Compra Ya
+                                    <i className="feather-icon icon-arrow-right ms-1"></i>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item" style={{backgroundImage: `url(${back2})`, backgroundSize: "cover", backgroundPosition: "center" , height:"540px", justifyContent:"start", alignItems:"center", borderRadius:"12px"}}>
+                        <div className="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center" style={{width:"400px", height:"320px" , marginLeft:"100px"}}>
+                            <span className="badge" style={{background:"#ffc107", color:"black"}}>Descuento por apertura del 50%</span>
+
+                            <h2 className="text-dark display-5 fw-bold mt-4">Alimentos siempre frescos</h2>
+                            <p className="lead">Descubre la nueva forma de comprar lo que necesitas para tu negocio!</p>
+                            <Link to="/lista_por_categorias/1">
+                                <button href="#!" className="btn btn-dark mt-3" tabIndex="0">
+                                    Compra Ya
+                                    <i className="feather-icon icon-arrow-right ms-1"></i>
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
 
-                <div className="carousel-item" style={{backgroundImage: `url(${back2})`, backgroundSize: "cover", backgroundPosition: "center" , height:"540px", justifyContent:"start", alignItems:"center", borderRadius:"12px"}}>
-                    <div className="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center" style={{width:"400px", height:"320px" , marginLeft:"100px"}}>
-                        <span className="badge" style={{background:"#ffc107", color:"black"}}>Opening Sale Discount 50%</span>
-
-                        <h2 className="text-dark display-5 fw-bold mt-4">SuperMarket For Fresh Grocery</h2>
-                        <p className="lead">Introduced a new model for online grocery shopping and convenient home delivery.</p>
-                        <Link to="/lista_por_categorias/1">
-                            <button href="#!" className="btn btn-dark mt-3" tabIndex="0">
-                                Shop Now
-                                <i className="feather-icon icon-arrow-right ms-1"></i>
-                            </button>
-                        </Link>
-                    </div>
-                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
         </div>
     )
 }
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,15 +1,13 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
-import { Link } from "react-router-dom";
 import { Carousel } from "../component/carrusel.jsx";
 import { Prom } from "../component/prom.jsx";
 import { Products_user } from "./products_user.jsx"
 import { Seccion } from "../component/seccion.jsx";
 import { Slide } from "../component/slide.jsx";
+import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
 	
 	return (
 		<div>	
